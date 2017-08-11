a508f757f84c983f9a816c971e20eb0c13763b51
tsukasadiary <tsukasadiary@gmail.com>
2017-08-12 01:36:42 +0900

    update

--- p800/ARC/E080/main.cpp
+++ p800/ARC/E080/main.cpp
@@ -11,16 +11,16 @@ template< typename T > using Vec = vector< T >;
 class SegmentTree {
 private:
 	int size__;
-	Vec< pii > data;
+	Vec< int > data;
 	
 	inline int left_t(int k) { return (k << 1) + 1; }
 	inline int right_t(int k) { return (k << 1) + 2; }
 	inline int center(int l, int r) { return (l + r) >> 1; }
 	
-	pii calc(pii d1, pii d2) { return min(d1, d2); }
+	int calc(int  d1, int  d2) { return min(d1, d2); }
 	
-	pii query(int a, int b, int k, int l, int r) {
-		if (r <= a || b <= l) return pii(1e9, 1e9);
+	int query(int a, int b, int k, int l, int r) {
+		if (r <= a || b <= l) return 1e9;
 		if (a <= l && r <= b) return data[k];
 		return calc(query(a, b, left_t(k), l, center(l, r)),
 					query(a, b, right_t(k), center(l, r), r));
@@ -31,10 +31,10 @@ public:
 	
 	SegmentTree(int n) {
 		for (size__ = 1; size__ < n; size__ <<= 1);
-		data.assign(2 * size__ - 1, pii(1e9, 1e9));
+		data.assign(2 * size__ - 1, 1e9);
 	}
 	
-	void update(int k, pii a) {
+	void update(int k, int  a) {
 		k += size__ - 1;
 		data[k] = a;
 		
@@ -44,30 +44,31 @@ public:
 		}
 	}
 	
-	pii query(int a, int b) { return query(a, b, 0, 0, size__); }
+	int query(int a, int b) { return query(a, b, 0, 0, size__); }
 };
 
-int N;
+int N, perm[200010];
 SegmentTree seg[2];
 
 Vec< pii > piece;
-Vec< int > nxt[100010];
-map< pii, int > piece_to_id;
+Vec< Vec< int > > nxt;
+map< pii , int > piece_to_id;
 
 void divide(int l, int r, int par) {
 	if (l >= r) return;
 	
-	pii a = seg[l % 2].query(l, r-1);
-	pii b = seg[(l + 1) % 2].query(a.second+1, r);
+	int a = seg[l % 2].query(l, r-1);
+	int b = seg[(l + 1) % 2].query(perm[a]+1, r);
 	
 	if (par != -1) nxt[par].push_back(piece.size());
-	piece_to_id[pii(a.first, b.first)] = piece.size();
-	piece.push_back(pii(a.first, b.first));
+	piece_to_id[pii(a, b)] = piece.size();
+	piece.push_back(pii(a, b));
 	
 	int cur = piece.size() - 1;
-	if (l < a.second) divide(l, a.second, cur);
-	divide(a.second + 1, b.second, cur);
-	if (b.second + 1 < r) divide(b.second + 1, r, cur);
+	nxt.push_back(Vec< int >());
+	if (l < perm[a]) divide(l, perm[a], cur);
+	divide(perm[a] + 1, perm[b], cur);
+	if (perm[b] + 1 < r) divide(perm[b] + 1, r, cur);
 }
 
 int main() {
@@ -77,7 +78,8 @@ int main() {
 	
 	for_(i,0,N) {
 		int p; cin >> p;
-		seg[i % 2].update(i, pii(p, i));
+		perm[p] = i;
+		seg[i % 2].update(i, p);
 	}
 	
 	divide(0, N, -1);
